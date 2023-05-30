a6f424904ff12bedc26ca61d88f102206368c7bd
tesseractx0FFFF <itkugit321@gmail.com>
2023-05-30 21:26:31 +0300

    added a few rules to the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,20 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
 };
